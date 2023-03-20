d42f1380d18bf7fd157e0aed35a42abddcbafcaa
mshubham0403 <75219721+mshubham0403@users.noreply.github.com>
2023-03-20 20:22:53 +0530

    resolve package-json error t2

--- client/src/router.js
+++ client/src/router.js
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App.js";
-import Chats from "./pages/Chats.js";
-import Home from "./pages/Home.js";
-import Room from "./pages/Room.js";
-import Sign from "./pages/SignUp.js";
-import Choice from "./pages/Choice.js";
-import NewRoom from "./pages/RoomDemo.js";
-import Profile from "./pages/Profile.js";
+import App       from "./App.js";
+import Chats     from "./pages/Chats.js";
+import Home      from "./pages/Home.js";
+import Room      from "./pages/Room.js";
+import Sign      from "./pages/SignUp.js";
+import Choice    from "./pages/Choice.js";
+import NewRoom   from "./pages/RoomDemo.js";
+import Profile   from "./pages/Profile.js";
 
 
 const router = createBrowserRouter([
